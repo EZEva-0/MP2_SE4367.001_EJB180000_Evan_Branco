14c950392fbefb9d2ddf113493b3473e53dea8c8
EZEva-0 <evanbrancowork@gmail.com>
2021-05-06 18:37:19 -0500

    MP2 Push One

---
+++ src/Graph.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Graph
+{
+	private int vertexes;
+	private LinkedList<Integer> graphList[];
+	
+	Assignment7(int vertex)
+	{
+		vertexes = vertex;
+		graphList = new LinkedList[vertex];
+	}
+	
+	public static void BFS(GraphG, Node s)
+	{
+		
+	}
+	
+	public static void main(String[] args) 
+	{
+		
+
+	}
+
+}
